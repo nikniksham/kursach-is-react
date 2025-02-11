007e0ce91315cef7cb1e214234b6e093cda30dfd
Niki <nikniksham@gmail.com>
2025-02-11 17:03:18 +0300

    настроен базовый фронт

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Добро пожаловать в React! 🚀</h1>
+          <p>Стартовая страница вашего нового проекта.</p>
+        </header>
+      </div>
+  );
+}
+
+export default App;
